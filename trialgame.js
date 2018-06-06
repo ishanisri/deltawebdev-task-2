fad4a5ef8752efc47670676bf29ef32afc898fac
ishanisri <32737620+ishanisri@users.noreply.github.com>
2018-06-06 14:06:02 +0530

    basic audio added

--- trialgame.js
+++ trialgame.js
@@ -1,17 +1,21 @@
-
-
-
-
-
-var framecheck=20;
+var framecheck=25;
 var myGamePiece;
 var myObstacles = [];
 var myScore;
 var gameOver;
 var count=0;
+var mySound;
+var myBackground;
+var countDown;
+var enemy=[];
+var myBells;
+var finalBell;
+var flagSound=0;
 
  function main(){
+    myGameArea.canvas.addEventListener("mousemove",move,false);
     myGameArea.clear();
+
     
     var x, height, gap, minHeight, maxHeight, minGap, maxGap,flag=0;
     for (i = 0; i < myObstacles.length; i += 1) {
@@ -23,6 +27,11 @@ var count=0;
             myGameArea.frameNo=0;
             gameOver.update();
             console.log("hi");
+            if(flagSound==1)
+            {myBackground.stop();
+                 mySound.play();
+
+               }
             if(count===0)
             {
             var p=document.getElementsByTagName("p")[0];
@@ -34,6 +43,7 @@ var count=0;
             btn.setAttribute("onclick","reset()"); 
             btn.setAttribute("style","background-color:red;border:1;text-align: center;display: inline-block;font-size: 16px;color:white;width:100px;height:50px");
             count++;
+           
 
             break;}
             
@@ -54,7 +64,11 @@ var count=0;
         gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
 
         myObstacles.push(new component(10, height, "pink", x, 0));
-        myObstacles.push(new component(10, x - height - gap, "pink", x, height + gap));}
+        myObstacles.push(new component(10, x - height - gap, "pink", x, height + gap));
+        enemy.push(new component(20,20,"white",10,10));
+        console.log("making enemies");
+
+            }
     
     for (i = myObstacles.length-1; i >0; i -= 1) {
         myObstacles[i].x += -2;
@@ -79,8 +93,80 @@ function startGame() {
     myGamePiece = new component( 5,"true","green",10,270/2,"circle");
     myScore = new component("30px", "Consolas", "white", 280, 40, "text");
     gameOver=new component("30px","Consolas","white",10,160,"text");
+    countDown=new component("100px","Consolas","white",480/2-30,270/2,"text");
+    mySound=new sound("explode.mp3");
+    myBackground=new sound("running.mp3");
+    myBells=new sound("bells.mp3");
+    finalBell=new sound("final bell.mp3");
+    
+    
+    if(window.confirm("do you want to play audio?"))
+    { myGameArea.start();
+        flagSound=1;var count = 4;
+function anim() {
+
+    if (count >= 0) {
+        myGameArea.clear();
+        if(count>0)
+        {countDown.text=count;
+            myBells.play();}
+
+    else
+        {countDown.text="GO!";
+         finalBell.play();
+     }
+    
+        countDown.update();
+
+        count--;
+        setTimeout(anim, 2000);
+
+    }
+    else {myBackground.play();
+    
+
+
+    
+    main();}
+        
+    }
+
+anim();
+}
+else{ flagSound=0;
     myGameArea.start();
+    var count=4;
+    function anim() {
+    if (count >= 0) {
+        myGameArea.clear();
+        if(count>0)
+        {countDown.text=count;
+            }
+
+    else
+        {countDown.text="GO!";
+         
+     }
+    
+        countDown.update();
+
+        count--;
+        setTimeout(anim, 2000);
+
+    }
+    
+    
+
+ else
+    
     main();
+        
+    }
+
+anim();
+}
+
+
 }
 
 var myGameArea = {
@@ -102,7 +188,8 @@ function component(width, height, color, x, y, type) {
     this.type = type;
     this.score = 0;
     this.width = width;
-    this.height = height;   
+    this.height = height;
+
     this.x = x;
     this.y = y;
     
@@ -157,7 +244,7 @@ function component(width, height, color, x, y, type) {
 
 
 
-myGameArea.canvas.addEventListener("mousemove",move,false);
+
 function move(event)
 {
         ctx=myGameArea.context;
@@ -177,10 +264,11 @@ function move(event)
 
  function reset()  
  {
-    
+    myGameArea.canvas.removeEventListener("mousemove", move);
     myGameArea.frameNo=0;
     myScore.text="SCORE:"+myGameArea.frameNo;
     myObstacles=[];
+    //myGameArea.canvas.addEventListener("mousemove",move,false);
 
     var button=document.getElementsByTagName("button")[0];
     button.parentNode.removeChild(button);
@@ -193,4 +281,19 @@ function move(event)
 
     
  }  
- 
+
+
+ function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }
+}
